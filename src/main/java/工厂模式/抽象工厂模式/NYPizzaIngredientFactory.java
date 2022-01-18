f5c5c3e6b9470c64d6d8eafcee86b88fdc802bb5
Wonderchn <a1073506500@QQ.COM>
2022-01-18 16:03:14 +0800

    工厂模式-抽象工厂方法

--- src/main/java/工厂模式/抽象工厂模式/NYPizzaIngredientFactory.java
+++ src/main/java/工厂模式/抽象工厂模式/NYPizzaIngredientFactory.java
@@ -4,27 +4,33 @@ package 工厂模式.抽象工厂模式;
  * Created by admin on 2022年1月18日
  */
 public class NYPizzaIngredientFactory implements PizzaIngredientFactory {
+    @Override
     public Dough createDough() {
         return new ThinCrustDough();
     }
 
+    @Override
     public Sauce createSauce() {
         return new MarinaraSauce();
     }
 
+    @Override
     public Cheese createCheese() {
         return new ReggianoCheese();
     }
 
+    @Override
     public Veggies[] createVeggies() {
         Veggies veggies[] = {new Garlic(), new Onion(), new Mushroom(), new RedPepper()};
         return veggies;
     }
 
+    @Override
     public Pepperoni createPepperoni() {
         return new SlicedPepperoni();
     }
 
+    @Override
     public Clams createClam() {
         return new FreshClams();
     }
